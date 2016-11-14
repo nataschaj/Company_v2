d95656ad5c07ed88da8caad7987373811be32363
Natascha Jakobsen <natascha_jakobsen@hotmail.com>
2016-11-14 13:40:33 +0100

    Senior Manager Class Added

---
+++ Sandbox/SeniorManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class SeniorManager : Manager
+    {
+        private int performanceLevel;
+
+        public SeniorManager(string name, int performanceLevel) : base(name)
+        {
+            this.performanceLevel = performanceLevel;
+        }
+
+        public override bool IsBonusPaidOut()
+        {
+            if (performanceLevel >= 6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+}
